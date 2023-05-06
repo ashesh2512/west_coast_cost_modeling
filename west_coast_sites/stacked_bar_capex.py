2bf1b8db4aa14180d7fdb344e19be81835a2f020
asharma <ashesh.sharma@nrel.gov>
2023-05-06 11:37:23 -0600

    update stacked bar charts

--- west_coast_sites/stacked_bar_capex.py
+++ west_coast_sites/stacked_bar_capex.py
@@ -4,20 +4,20 @@ import pandas as pd
 plt.rcParams["font.family"] = "Times New Roman"
 
 scenarios = (
-    "Central CA\n (San Luis)",
-    "Central CA\n (Long Beach)",
-    "Central CA\n (Humboldt)",
-    "Northern CA\n (Humboldt)",
-    "Northern CA\n (San Luis)",
-    "Southern OR\n (Coos Bay)",
-    "Central OR\n (Coos Bay)",
-    "Southern WA\n (Grays Harbor)",
+    "Central CA\n (Central CA)",
+    "Central CA\n (Southern CA)",
+    "Central CA\n (Northern CA)",
+    "Northern CA\n (Northern CA)",
+    "Northern CA\n (Central CA)",
+    "Southern OR\n (OR coast)",
+    "Central OR\n (OR coast)",
+    "Southern WA\n (WA coast)",
 )
 capex = {
-    "System CapEx": np.array([1659, 1659, 1659, 1524, 1524, 1524, 1466, 1605]),
-    "Turbine CapEx": np.array([1306, 1306, 1306, 1306, 1306, 1306, 1306, 1306]),
-    "Installation CapEx": np.array([393, 413, 429, 419, 538, 485, 386, 390]),
-    "Soft CapEx": np.array([645, 645, 645, 645, 645, 645, 645, 645]),
+    "System CapEx": np.divide(np.array([1613, 1613, 1613, 1475, 1475, 1502, 1434, 1562]), 1),
+    "Turbine CapEx": np.divide(np.array([1306, 1306, 1306, 1306, 1306, 1306, 1306, 1306]), 1),
+    "Installation CapEx": np.divide(np.array([464, 510, 525, 540, 597, 589, 496, 524]), 1),
+    "Soft CapEx": np.divide(np.array([645, 645, 645, 645, 645, 645, 645, 645]), 1),
 }
 width = 0.5
 
@@ -32,12 +32,14 @@ for c in ax.containers:
 
     # Optional: if the segment is small or 0, customize the labels
     labels = [v.get_height() if v.get_height() > 0 else '' for v in c]
+
+    labels = [f'{val:.0f}' for val in labels]
     
     # remove the labels parameter if it's not needed for customized labels
     ax.bar_label(c, labels=labels, label_type='center')
 
 ax.set_ylim([0, 6000])
-ax.set_ylabel("$M")
+ax.set_ylabel("$/kW")
 ax.legend(loc="upper right")
 
 plt.show()
